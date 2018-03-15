ba478728413f38c2d2f80c1c2f7c4a054015bc7a
Betu <Home@Betus-MacBook-Air.local>
2018-03-15 19:52:41 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict"
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//MIDDLE TO DEFINE FOLDER TO STATIC FILES
+app.use(express.static('public'))
+
+//CAPTURE ROUTE
+app.get('/', function(req, res){
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(3000, function(){
+    console.log('app is listening')
+})
